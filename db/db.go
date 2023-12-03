57d2b88d7af7b4b4cbe2b5a6c2a26dfda37e65a3
gosmach1ne <1234567yhb@gmail.com>
2023-12-03 10:59:19 +0200

    add .env, minor optimization

--- db/db.go
+++ db/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"gin_test/logs"
+	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 	"go.uber.org/zap"
 	"gorm.io/driver/postgres"
@@ -18,9 +19,12 @@ var (
 )
 
 func init() {
+	err := godotenv.Load()
+	if err != nil {
+		logs.Logger.Error("", zap.Error(err))
+	}
 	pgConnStr := fmt.Sprintf("user=%s password=%s dbname=%s host=127.0.0.1 sslmode=disable",
 		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
-	var err error
 	Db, err = gorm.Open(postgres.Open(pgConnStr), &gorm.Config{})
 	if err != nil {
 		logs.Logger.Error("", zap.Error(err))
